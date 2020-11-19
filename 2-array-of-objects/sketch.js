461aec0bf2f93219084ef111c7f96288a278a045
samkapl <74516473+samkapl@users.noreply.github.com>
2020-11-18 17:42:09 -0800

    Completed worksheet

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -1,7 +1,8 @@
 
+
 //create an empty array called balls
 
-let balls = [];
+let mushrooms = [];
 
 function setup() {
   createCanvas(800, 400);
@@ -12,34 +13,43 @@ function draw(){
 	background(220);
 
 //	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	    balls[i].drawBall();
-      balls[i].moveBall();
+	for (let i = 0; i < mushrooms.length; i++) {
+	    mushrooms[i].drawMushroom();
+      mushrooms[i].moveMushroom();
 	  }
 }
 
 function keyPressed(){
-  // todo: 
-  //every time you push a key, make a new ball from the ball class and add it to the balls array
+
+  let  m = new Mushroom(random(0,800), random(0,400),"red");
+  mushrooms.push(m);
+  print(mushrooms);
 }
 
 //ball class from which to create new balls with similar properties.
-class Ball {
+class Mushroom {
 
-	constructor(x,y){ //every ball needs an x value and a y value
+	constructor(x,y,color){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
+        this.color = color
 	}
 
-	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
-    		fill("red");
-		    ellipse(this.x,this.y,10,10);
+	drawMushroom(){  // draw a ball on the screen at x,y
+    		noStroke();
+        fill("tan");
+        ellipse(this.x, this.y + 20, 20, 30);
+        fill(255);
+        ellipse(random(this.x,25),random(this.y,10), 10, 10);
+        fill(this.color);
+        ellipse(this.x,this.y,50,20);
+
+
 	}
 
-	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
-		this.y = this.y+.5;
+	moveMushroom(){ //update the location of the ball, so it moves across the screen
+		this.x = this.x+.5;
+		this.y = this.y+.2;
 	}
 
 
